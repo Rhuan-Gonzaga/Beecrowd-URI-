e88e350e2f0ec5ca9d7edb5b26d2f3b46afcd2bd
Rhuan Gonzaga <62728188+Rhuan-Gonzaga@users.noreply.github.com>
2022-06-07 09:25:25 -0300

    Sequencia IJ 3

---
+++ bee1096.py
@@ -0,0 +1,21 @@
+'''
+1096 - Sequencia IJ 2
+Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+
+Entrada
+Não há nenhuma entrada neste problema.
+
+Saída
+Imprima a sequencia conforme exemplo abaixo
+
+'''
+count = 0
+i = 1
+j = 7
+while count < 5:
+    for n in range(3):
+        print(f"I={i} J={j}")
+        j-=1
+    j=7
+    i+=2
+    count+=1
