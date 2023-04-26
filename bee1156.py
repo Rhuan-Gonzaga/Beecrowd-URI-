f8527510bd4ba488fdc03380cc862135b7f96c62
Rhuan-Gonzaga <rhuangonzaga22@gmail.com>
2023-04-26 18:30:06 -0300

    Sequência S II

--- bee1156.py
+++ bee1156.py
@@ -1,8 +1,7 @@
-
-"""
-1155 - Sequência S
+""" 
+1156 - Sequência S II
 Escreva um algoritmo para calcular e escrever o valor de S, sendo S dado pela fórmula:
-S = 1 + 1/2 + 1/3 + … + 1/100
+S = 1 + 3/2 + 5/4 + 7/8+… + 39/?
 
 Entrada
 Não há nenhuma entrada neste problema.
@@ -10,13 +9,13 @@ Não há nenhuma entrada neste problema.
 Saída
 A saída contém um valor correspondente ao valor de S.
 O valor deve ser impresso com dois dígitos após o ponto decimal.
-    
 """
 
+s = 0
+j = 1
+for i in range(1,40,2):
+    s = float(s + i / j)
+    j = j * 2
 
-s = int(0)
 
-for i in range(1,101):
-    s = s + (1/i)
-
-print(round(s,2))
+print("{:.2f}".format(s))
