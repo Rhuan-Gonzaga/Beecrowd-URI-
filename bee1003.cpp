81b08c5002832a9d24b2865431dc02cceb29769e
Rhuan-Gonzaga <rhuangonzaga22@gmail.com>
2023-11-20 19:47:14 -0300

    Soma Simples

---
+++ bee1003.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A;
+    int B;
+    
+    cin >> A;
+    cin >> B;
+    
+    int soma = A+B;
+    
+    cout <<"SOMA = "<< soma << endl;
+}
