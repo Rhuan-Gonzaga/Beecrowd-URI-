0f1972d9748a61098396b621bb9df12db73ed6e5
Rhuan-Gonzaga <rhuangonzaga22@gmail.com>
2023-11-20 19:54:43 -0300

    Produto Simples

---
+++ bee1004.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A;
+    int B;
+    
+    cin >> A;
+    cin >> B;
+    
+    int produto = A*B;
+    
+    cout <<"PROD = "<< produto << endl;
+}
